2a65ab865058a825cd0d38421cc3660a8a4d1518
JosephMugo <joemu18@gmail.com>
2020-11-07 14:03:59 -0600

    navbar for mobile is up to spec

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,18 +7,18 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-md navbar-light">
             <div className="navbar-header">
                 <img className="navbar-brand" src={logo} alt=""/>
-                <ul class="navbar-nav collapse">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">OUR COMPANY</a>
+                <ul className="navbar-nav collapse">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">OUR COMPANY</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">LOCATIONS</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">LOCATIONS</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">CONTACT</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">CONTACT</a>
                     </li>
                 </ul>
-                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> 
+                <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> 
                     <div className="menu-btn">
                         <div className="bar1"></div>
                         <div className="bar2"></div>
@@ -26,16 +26,16 @@ const NavBar = () => {
                     </div>
                 </button>
             </div>
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">OUR COMPANY</a>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">OUR COMPANY</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">LOCATIONS</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">LOCATIONS</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">CONTACT</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">CONTACT</a>
                     </li>
                 </ul>
             </div>
